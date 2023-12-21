8c6194265d51102630b06fd8e74d808275781759
pavanmc <pavanmchavan1995@gmail.com>
2023-12-21 22:08:24 +0530

    check files

---
+++ js/script.js
@@ -0,0 +1,44 @@
+// const nav = document.querySelector('#header')
+// const foot = document.querySelector('#footer')
+
+// fetch('header.html')
+//     .then(res => res.text())
+//     .then(data => {
+//         nav.innerHTML = data
+//     })
+
+// fetch('footer.html')
+//     .then(res => res.text())
+//     .then(data => {
+//         foot.innerHTML = data
+//     })
+
+// ***************************************************************
+// *************************** Footer ***************************
+// ***************************************************************
+
+
+
+
+// function includeHTML() {
+//     const headerElement = document.getElementById('header');
+//     const footerElement = document.getElementById('footer');
+
+
+// Include header (navigation)
+// fetch('header.html')
+//     .then(response => response.text())
+//     .then(data => {
+//         headerElement.innerHTML = data;
+//     });
+
+
+// Include footer
+//     fetch('footer.html')
+//         .then(response => response.text())
+//         .then(data => {
+//             footerElement.innerHTML = data;
+//         });
+// }
+
+// document.addEventListener('DOMContentLoaded', includeHTML);
